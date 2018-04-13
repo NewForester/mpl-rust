24110f1f53e1ba440efd9b34f4d8297a78fc928e
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-04-13 17:46:03 +0100

    revise solution for the Exercism scabble-score exercise

--- exercism/scrabble-score.rs
+++ exercism/scrabble-score.rs
@@ -1,70 +1,69 @@
 //! Exercism Rust Track:  a solution for the scrabble-score exercise
 
+#[macro_use]
+extern crate lazy_static;
+
 use std::collections::HashMap;
 
-// Function score() takes a word or string and returns what it would score in (English) Scrabble
-pub fn score(word: &str) -> i32 {
+// Macro SCORES builds a HashMap for static use that maps (English) letters to Scrabble scores
+lazy_static! {
+    static ref SCORES: HashMap<char,u8> = {
 
-    let letters = [
-        'A', 'E', 'I', 'O', 'U',
-        'L', 'N', 'R', 'S', 'T',
-        'D', 'G',
-        'B', 'C', 'M', 'P',
-        'F', 'H', 'V', 'W', 'Y',
-        'K',
-        'J', 'X',
-        'Q', 'Z',
-    ];
+        let mut scrabble_scores: HashMap<char,u8> = HashMap::new();
 
-    let letter_scores = [
-        1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1,
-        2, 2,
-        3, 3, 3, 3,
-        4, 4, 4, 4, 4,
-        5,
-        8, 8,
-        10, 10,
-    ];
+        let problem_statement = [
+            ("A, E, I, O, U",   1),
+            ("L, N, R, S, T",   1),
+            ("D, G",            2),
+            ("B, C, M, P",      3),
+            ("F, H, V, W, Y",   4),
+            ("K",               5),
+            ("J, X",            8),
+            ("Q, Z",           10),
+        ];
 
-    let scores: HashMap<_, _> = letters.iter().zip(letter_scores.iter()).collect();
+        for pair in problem_statement.iter() {
+            let score = pair.1;
 
-    let mut sum = 0;
-
-    for cc in word.to_uppercase().chars() {
-        match scores.get(&cc) {
-            None => (),
-            Some(value) => sum = sum + **value,
+            for letter in pair.0.chars() {
+                if letter.is_alphabetic() {
+                    scrabble_scores.insert(letter, score);
+                }
+            }
         }
-    }
 
-    sum
+        scrabble_scores
+    };
+}
+
+// Function letter_score() takes a single letter and returns its (English) Scrabble score
+fn letter_score(cc: char) -> i32 {
+    match SCORES.get(&cc) {
+        None => 0,
+        Some(value) => (*value) as i32,
+    }
+}
+
+// Function score() takes a word and returns its (English) Scrabble score
+pub fn score(word: &str) -> i32 {
+    word.to_ascii_uppercase().chars().map(letter_score).sum()
 }
 
 //
-// My solution is different from the majority of other solutions examined.
-// Mine uses a 'dict':  all others used a large match statement (and they
-// didn't use a macro).
+// So, use .to_ascii_uppercase() instead of .to_uppercase() and the German test
+// passes.  Do not ask why.
 //
-// It seems you cannot declare a 'literal' HashMap in Rust.  My fiddling to
-// create one had to use .zip and stuff so I should have used .map and stuff
-// to finish the job.  Perhaps
+// Moving the lookup into a separate routine hides the ugliness of the HashMap
+// lookup.  In its final form, it gives us a default value for unrecognised letters.
 //
-// .map(| cc | **scores.get(**&cc).sum())
+// This allows the simple use of map() and sum().  Nothing fancy but just plain,
+// simple to reason about, code.
 //
-// but the return types of the .get routine is ugly and I might have had no end
-// of trouble with it.  I expect I would have had to wrap it up in function.
+// It seems from the crate documentation that you can declare and initialise a
+// literal HashMap but by this time I was committed to finding a way of having
+//     a) static data at the file level
+//     b) initialising it at run-time
 //
-// Several of the other solutions used .map and stuff with the syntax I am now
-// familiar with but do not yet understand.
-//
-// In the middle they invoke a routine to score a single letter.  Reminds me of
-// good coding practice as I once practised it.
-//
-// Some other solutions use to_ascii_lowercasee / to_ascii_uppercase.  I wonder
-// if that solves the German letters problem.  I tried using the Unicode create
-// but was told by cargo that it did not exist.
-//
-// Finally, I wonder if I could not use the nice idea I saw on the Go track by
-// tewe to initialise my hash map.
+// The first is part of the language but discouraged.  The second uses a crate
+// that the language designers would, no doubt, frown upon.
 //
