4a609757b90ca4da0315d20aee2a8bb24eb0ed05
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-04-21 09:09:08 +0100

    revise solution for the Exercism clock exercise

--- exercism/clock.rs
+++ exercism/clock.rs
@@ -3,72 +3,28 @@
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub struct Clock {
-    hh: i16,
-    mm: i16,
+    time: i16,
 }
 
 impl Clock {
     /// Function new() allows an inititialised clock to be created
     pub fn new (hh: i16, mm: i16) -> Clock {
+        let mut time = (hh * 60 + mm) % (24 * 60);
 
-        let mut clock = Clock {hh, mm};
+        if time < 0 {time += 24 * 60;}
 
-        clock.normalise();
-
-        clock
+        Clock {time}
     }
 
     /// Function add_minutes() allows the clock time to be adjusted
-    pub fn add_minutes(mut self, mm: i16) -> Clock {
-
-        self.mm += mm;
-
-        self.normalise();
-
-        self
-    }
-
-    /// Function normalise() ensures the internal representation is suitable for formatting and comparison
-    fn normalise(&mut self) -> () {
-        let time = self.hh * 60 + self.mm;
-
-        self.hh = time / 60 % 24;
-        self.mm = time % 60;
-
-        if self.mm < 0 {
-            self.hh -= 1;
-            self.mm += 60;
-        }
-        if self.hh < 0 {
-            self.hh += 24;
-        }
+    pub fn add_minutes(self, mm: i16) -> Clock {
+        Clock::new(0, self.time + mm)
     }
 }
 
 impl std::string::ToString for Clock {
     /// Function to_string() formats the clock time for printing
     fn to_string(&self) -> String {
-
-        format!("{:02}:{:02}", self.hh, self.mm)
+        format!("{:02}:{:02}", self.time / 60, self.time % 60)
     }
 }
-
-//
-// This solution follows the form of the others examined.  There is little
-// choice.
-//
-// Half stored the time as a large minute value, half as hour and minutes.
-// It makes little difference.
-//
-// All stored normalised values, several had separate normalise routines.
-//
-// All implemented ToString but only one implemented PartialEq.
-//
-// Mine was unique in returning a modified Clock in the Add routine rather
-// than a new Clock.  Probably mine is incorrect.
-//
-// If I do this again I suggest return a new clock from Add.  Then normalise
-// need not be a separate routine.  Changing the structure to hold just
-// minutes should enable the normalisation code to be shortened by using
-// 24 * 60.
-//
